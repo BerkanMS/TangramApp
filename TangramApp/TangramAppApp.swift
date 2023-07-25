cf72320935a1846e1b3f9316255aaeacc9e92ee6
Berkan <97912597+BerkanMS@users.noreply.github.com>
2023-07-25 14:05:22 +0300

    Initial Commit

---
+++ TangramApp/TangramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangramAppApp.swift
+//  TangramApp
+//
+//  Created by Berkan M. Sevinç on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TangramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
